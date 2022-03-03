bd89b0ceb67fd346ef58aa4a672e8da0aba77009
Ewan Pandelus <ewanpandelus@hotmail.com>
2022-03-03 12:03:25 +0000

    Add bounds to camera

--- Assets/Scripts/Camera/MapPan.cs
+++ Assets/Scripts/Camera/MapPan.cs
@@ -4,13 +4,16 @@ using UnityEngine;
 
 public class MapPan : MonoBehaviour
 {
-    private float mouseX;
-    private float mouseY;
+    private float mouseX, mouseY;
+  
     [SerializeField] private float panSensitivity = 0.2f;
     [SerializeField] private float scrollSensitivity = 10;
     [SerializeField] private float smoothSpeed;
-    private Vector3 cameraPos;
+    [SerializeField] private float leftPanBound, rightPanBound, upperPanBound, lowerPanBound;
+    [SerializeField] private float lowerScrollBound, upperScrollBound;
+    private Vector3 cameraPos, smoothPos;
     private Camera camera;
+   
     private void Start()
     {
         cameraPos = transform.position;
@@ -19,25 +22,58 @@ public class MapPan : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float scrollDelta = Input.GetAxis("Mouse ScrollWheel");
+
+        CalcNewCameraScrollPosition();
         if (Input.GetMouseButton(0))
         {
             mouseX = Input.GetAxis("Mouse X");
             mouseY = Input.GetAxis("Mouse Y");
-            cameraPos += transform.right * (mouseX * -1) * panSensitivity * camera.orthographicSize;
-            cameraPos += transform.up * (mouseY * -1) * panSensitivity * camera.orthographicSize;
-            Vector3 smoothPost = Vector3.Lerp(transform.position, cameraPos, smoothSpeed);
-            transform.position = smoothPost;
+            CalcNewCameraPanPosition();
+          
+            smoothPos =  Vector3.Lerp(transform.position, cameraPos, smoothSpeed);
+            transform.position = smoothPos;
         }
-        if (scrollDelta > 0 && camera.orthographicSize > 8)
+      
+    }
+    private void CalcNewCameraScrollPosition()
+    {
+        float scrollDelta = Input.GetAxis("Mouse ScrollWheel");
+        if (scrollDelta > 0 && camera.orthographicSize > lowerScrollBound)
         {
             camera.orthographicSize -= Time.deltaTime * scrollSensitivity;
         }
-        if (scrollDelta < 0 && camera.orthographicSize < 24)
+        if (scrollDelta < 0 && camera.orthographicSize < upperScrollBound)
         {
             camera.orthographicSize += Time.deltaTime * scrollSensitivity;
         }
     }
+    private void CalcNewCameraPanPosition()
+    {
+        cameraPos += transform.right * (mouseX * -1) * panSensitivity * camera.orthographicSize;
+        cameraPos += transform.up * (mouseY * -1) * panSensitivity * camera.orthographicSize;
+        cameraPos = AccountForPanBounds(cameraPos);
+    }
+    private Vector3 AccountForPanBounds(Vector3 _cameraPos)
+    {
+        if (_cameraPos.x > rightPanBound)
+        {
+            _cameraPos.x = rightPanBound;
+        }
+        if (_cameraPos.x < -leftPanBound)
+        {
+            _cameraPos.x = -leftPanBound;
+        }
+        if (_cameraPos.y > upperPanBound)
+        {
+            _cameraPos.y = upperPanBound;
+        }
+        if (_cameraPos.y < -lowerPanBound)
+        {
+            _cameraPos.y = -lowerPanBound;
+        }
+        return _cameraPos;
+    }
+
     public void SetPosition(Vector3 _pos, float _orthoSize)
     {
         transform.position = _pos;
